b87e64318ea077ed03b8feec32270db9736ea8f6
ihsan-husnul <ihsan.husnul@kiranatama.com>
2013-12-05 15:55:49 +0700

    Handle if value is binary type

--- lib/seed_dump/dump_methods.rb
+++ lib/seed_dump/dump_methods.rb
@@ -125,7 +125,7 @@ class SeedDump
       elsif value.is_a?(Date) || value.is_a?(Time)
         %("#{value.to_s(:db)}")
       elsif defined?(UUIDTools::UUID)=='constant' && value.is_a?(UUIDTools::UUID)
-        "#{value.to_param}"
+        "UUIDTools::UUID.serialize('#{value.to_param}')"
       else
         value.inspect
       end
